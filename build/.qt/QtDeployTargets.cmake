4094b1a01170cda646cdff9257dda906d00783fe
necto <134859063+arnecto@users.noreply.github.com>
2026-07-15 09:04:35 +0300

    Add gitignore, license, readme; remove build artifacts

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_YtDlpGui_FILE D:/YtDlpGui/build/YtDlpGui.exe)
-set(__QT_DEPLOY_TARGET_YtDlpGui_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_YtDlpGui_RUNTIME_DLLS D:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
